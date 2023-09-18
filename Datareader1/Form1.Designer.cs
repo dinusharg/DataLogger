4a0409c01ae1ce3a461f02909d3929eeb613ed5a
Dinusha Gunathilaka <dinusharg@gmail.com>
2023-09-18 12:58:56 +0530

    Initial commit.

---
+++ Datareader1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Datareader1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(299, 86);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(330, 319);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Datareader1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+
+namespace Datareader1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort mySerialPort;
+        StreamWriter logFile;
+        string fileName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime timeStamp = DateTime.Now;
+            string dt = timeStamp.ToString("yyyy_MM_dd_HH_mm_ss");
+            fileName = dt + ".csv";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mySerialPort = new SerialPort("COM8", 9600);
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                mySerialPort.Open();
+
+            }
+            catch (IOException ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string line = mySerialPort.ReadLine();
+            richTextBox1.AppendText(line);
+        }
+
+        private void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            //Console.WriteLine("Data Received:");
+            //Console.Write(indata);
+            ConsoleUpdate(indata);
+        }
+
+        public void ConsoleUpdate(string text)
+        {
+            if(richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action<string>(ConsoleUpdate), text);
+            }
+            else
+            {
+                richTextBox1.AppendText(text);
+                writeCSV(text);
+            }
+
+           
+        }
+
+        public void writeCSV(string text)
+        {
+            DateTime timeStamp = DateTime.Now;
+            //string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string dt = timeStamp.ToString("dd / MM / yyyy HH: mm: ss.fff");
+            logFile = new StreamWriter(fileName, true);
+            logFile.Write(dt + ","+ text);
+            logFile.Close();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime timeStamp = DateTime.Now;
+            string dt = timeStamp.ToString("yyyy_MM_dd_HH_mm_ss");
+            fileName = dt + ".csv";
+
+        }
+    }
+}
