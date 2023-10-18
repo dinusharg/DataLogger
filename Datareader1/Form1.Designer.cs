a645391663e019db4e474266ac9d9d976ccd6705
Dinusha Gunathilaka <dinusharg@gmail.com>
2023-10-18 16:13:36 +0530

    connect and disconnect button added.

--- Datareader1/Form1.Designer.cs
+++ Datareader1/Form1.Designer.cs
@@ -30,37 +30,22 @@ namespace Datareader1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(43, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 58);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(43, 154);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 49);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(299, 86);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 36);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(330, 319);
+            this.richTextBox1.Size = new System.Drawing.Size(520, 389);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
@@ -70,27 +55,92 @@ namespace Datareader1
             this.timer1.Interval = 300000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(87, 6);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(93, 24);
+            this.cmbPort.TabIndex = 3;
+            this.cmbPort.SelectedIndexChanged += new System.EventHandler(this.cmbPort_SelectedIndexChanged);
+            this.cmbPort.Click += new System.EventHandler(this.cmbPort_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(186, 2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(106, 31);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(544, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 16);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(298, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(107, 31);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "COM Port";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(544, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbPort);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Data Logger";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Datareader1/Form1.cs
+++ Datareader1/Form1.cs
@@ -15,66 +15,67 @@ namespace Datareader1
 {
     public partial class Form1 : Form
     {
-        SerialPort mySerialPort;
+ 
+        SerialPort COMPort = new SerialPort();
+
         StreamWriter logFile;
         string fileName = "";
+        string logData = "";
 
         public Form1()
         {
             InitializeComponent();
+            if (Directory.Exists("data") == false)
+            {
+                Directory.CreateDirectory("data");
+            }
+
+            timer1.Interval = 15 * 60  * 1000;
+
             DateTime timeStamp = DateTime.Now;
             string dt = timeStamp.ToString("yyyy_MM_dd_HH_mm_ss");
-            fileName = dt + ".csv";
+            fileName ="data/"+ dt + ".csv";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            mySerialPort = new SerialPort("COM8", 9600);
-            
-
+            string[] arrayComPortNames = null;
+            arrayComPortNames = SerialPort.GetPortNames();
+            cmbPort.DataSource = arrayComPortNames;
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                
-                mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-                mySerialPort.Open();
 
-            }
-            catch (IOException ex)
-            {
 
-            }
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            string line = mySerialPort.ReadLine();
-            richTextBox1.AppendText(line);
-        }
 
         private void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
             string indata = sp.ReadExisting();
-            //Console.WriteLine("Data Received:");
-            //Console.Write(indata);
-            ConsoleUpdate(indata);
+            logData = indata;
+
+            DateTime timeStamp = DateTime.Now;
+            string dt = timeStamp.ToString("dd / MM / yyyy HH: mm: ss.fff");
+            string log_date = timeStamp.ToString("dd / MM / yyyy");
+            string log_time = timeStamp.ToString("HH: mm: ss.fff");
+
+            string dataLog = dt+" : " + indata;
+
+            ConsoleUpdate(dataLog);
         }
 
-        public void ConsoleUpdate(string text)
+        public void ConsoleUpdate(string dataLog)
         {
-            if(richTextBox1.InvokeRequired)
+            
+            if (richTextBox1.InvokeRequired)
             {
-                richTextBox1.Invoke(new Action<string>(ConsoleUpdate), text);
+                richTextBox1.Invoke(new Action<string>(ConsoleUpdate), dataLog);
             }
             else
             {
-                richTextBox1.AppendText(text);
-                writeCSV(text);
+                richTextBox1.AppendText(dataLog);
+                writeCSV(logData);
             }
 
            
@@ -82,21 +83,92 @@ namespace Datareader1
 
         public void writeCSV(string text)
         {
-            DateTime timeStamp = DateTime.Now;
-            //string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
-            string dt = timeStamp.ToString("dd / MM / yyyy HH: mm: ss.fff");
-            logFile = new StreamWriter(fileName, true);
-            logFile.Write(dt + ","+ text);
-            logFile.Close();
+            if (text.Length > 0)
+            {
+                if (text.Substring(0, 1) == "#")
+                {
+                    DateTime timeStamp = DateTime.Now;
+                    string dt = timeStamp.ToString("dd / MM / yyyy HH: mm: ss.fff");
+                    string log_date = timeStamp.ToString("dd / MM / yyyy");
+                    string log_time = timeStamp.ToString("HH: mm: ss.fff");
 
+                    string logText = log_date + "," + log_time + "," + text;
+
+
+                    logFile = new StreamWriter(fileName, true);
+                    logFile.Write(logText);
+                    logFile.Close();
+                }
+            }
+         
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             DateTime timeStamp = DateTime.Now;
             string dt = timeStamp.ToString("yyyy_MM_dd_HH_mm_ss");
-            fileName = dt + ".csv";
+            fileName ="data/"+ dt + ".csv";
 
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (COMPort.IsOpen == true)
+                {
+                    COMPort.Close();
+                }
+
+                COMPort.PortName = cmbPort.Text;
+                COMPort.BaudRate = 9600;
+
+                COMPort.Open();
+                toolStripStatusLabel1.Text = COMPort.PortName + " connected.";
+                COMPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                btnConnect.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                toolStripStatusLabel1.Text = "Com port connection error";
+                MessageBox.Show("Com port connection error\n" + ex.Message.ToString());
+                btnConnect.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (COMPort.IsOpen == true)
+                {
+                    COMPort.Close();
+
+                    toolStripStatusLabel1.Text = "Com port disconeected.";
+
+                    btnConnect.Enabled = true;
+                    btnStop.Enabled = false;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void cmbPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbPort_Click(object sender, EventArgs e)
+        {
+            string[] arrayComPortNames = null;
+            arrayComPortNames = SerialPort.GetPortNames();
+            cmbPort.DataSource = arrayComPortNames;
         }
     }
 }
